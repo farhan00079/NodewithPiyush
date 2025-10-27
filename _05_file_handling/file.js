653a7cbd34fdbf7f384ae6f6aa99919d604ba851
Farhan Alam <97759342+farhan00079@users.noreply.github.com>
2025-10-27 11:43:47 +0000

    far

---
+++ _05_file_handling/file.js
@@ -0,0 +1,40 @@
+console.log('farhan alam');
+
+const { log } = require("console");
+const fs = require("fs");
+
+// ~~~~ sync ~~~
+fs.writeFileSync("./test.txt", "Hello Farhan");
+
+// ~~~~ async ~~~~
+fs.writeFile("./text.txt", "Hello World Async", (err)=> {
+
+})
+
+// ~~~~~~~ sync ~~~~~~~~
+// const result = fs.readFileSync("./contact.js", "utf-8");
+// console.log(result);
+
+// ~~~~~~~ async ~~~~~~~~
+fs.readFile("./contact.js", "utf-8", (err, result) => {
+    if(err){
+        console.log("Error", err);    
+    }
+    else {
+        console.log(result);
+        
+    }
+});
+
+fs.appendFileSync("./test.txt", `${Date.now()} Hey There\n`);
+
+fs.copyFileSync("test.txt", "copy.txt");
+
+fs.unlinkSync("./text.txt");
+
+const stats = fs.statSync("./test.txt"); // file info deta hai
+console.log(stats.isFile()); // true agar file hai, false agar nahi
+
+fs.mkdirSync("my-docs");
+
+fs.mkdirSync("my-docs/a/b", { recursive: true});
--- _50_rough/rough.js
+++ _50_rough/rough.js
@@ -1,7 +1,3 @@
-const math = require("./math");
-console.log("fahan");
-
-console.log("math value is: " + math.add(4,5));
-console.log("math value is: " + math.sub(4,5));
-
+const { error } = require("console");
+const fs = require("fs");
 
