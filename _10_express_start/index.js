f12391c00c129ec38d6e71c8ae8b7b435ebafe3f
Farhan Alam <97759342+farhan00079@users.noreply.github.com>
2025-11-05 15:10:25 +0000

    far

---
+++ _10_express_start/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) =>{
+    return res.send("hello from home page");
+})
+
+app.get("/about", (req, res) =>{
+    return res.send(`hello ${req.query.name}`);
+})
+
+app.listen(8000, () =>{
+    console.log("server started");
+    
+})
