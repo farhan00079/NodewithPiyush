de8b2c982b3d28a105a2045993a2f1d4c091af6f
Farhan Alam <97759342+farhan00079@users.noreply.github.com>
2025-11-10 14:41:30 +0000

    far

--- _13_restapi_projectOne/index.js
+++ _13_restapi_projectOne/index.js
@@ -17,9 +17,48 @@ app.get("/users", (req, res) => {
     res.send(html);
 })
 
+// ~~~~~~ Rest Apis ~~~~~
 app.get("/api/users", (req, res) => {
     return res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+});
+
+app.post("/api/users", (req, res) => {
+    // TO DO : Create new user
+    return res.json({status: "pending"});
+});
+
+app.patch("/api/users/:id", (req, res) => {
+    // TO DO : Edit the user with id
+    return res.json({status: "pending"});
+});
+
+app.delete("/api/users", (req, res) => {
+    // TO DO : Delete the user with id 
+    return res.json({status: "pending"});
+});
+
+// ~~~~ Single route create ~~~~~~~~~
+app.route("/api/users/:id")
+.get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
 })
+.put((req, res) => {
+    // TO DO : Edit the user with id
+    return res.json({status: "pending"});
+})
+delete((req, res) => {
+    // TO DO : Delete the user with id 
+    return res.json({status: "pending"});
+})
+
 
 
 
