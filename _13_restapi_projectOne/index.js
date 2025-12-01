7f44123ec19daf8aef2917f2a6985a6365670663
Farhan Alam <97759342+farhan00079@users.noreply.github.com>
2025-12-01 08:23:06 +0000

    far

--- _13_restapi_projectOne/index.js
+++ _13_restapi_projectOne/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 3500;
+
+// ~~~~~~ Middleware -> Plugin ~~~~~~~~~
+app.use(express.urlencoded({extended : false }));
 
 // ~~~~ Route ~~~~~~~~~
 app.get("/users", (req, res) => {
@@ -30,7 +34,12 @@ app.get("/api/users/:id", (req, res) => {
 
 app.post("/api/users", (req, res) => {
     // TO DO : Create new user
-    return res.json({status: "pending"});
+    const body = req.body;
+    users.push({...body, id: users.length + 1});
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+          return res.json({status: "pending"});
+    })
+
 });
 
 app.patch("/api/users/:id", (req, res) => {
